e0390db0b21c2779a58234ee33bdfc2abeb2c1f6
adolfogomes <Adolfo.Gomes@ibm.com>
2026-03-19 20:42:59 -0300

    config: setup files

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-console": "off",
+
+      eqeqeq: "error",
+      curly: "error",
+
+      "no-var": "error",
+      "prefer-const": "error",
+
+      "arrow-body-style": ["error", "as-needed"],
+
+      "no-multiple-empty-lines": ["error", { max: 1 }],
+      "no-trailing-spaces": "error",
+
+      "consistent-return": "error",
+
+      "no-implicit-globals": "error",
+
+      "no-restricted-globals": ["error", "event"],
+    },
+  },
+]);
